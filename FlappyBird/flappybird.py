8bc61248904f585ac12cd8cd6cd01b13ca9abc5a
Christian Thompson <wynand1004@gmail.com>
2019-10-12 12:41:37 +0900

    Initial Commit
    Live coding October 12, 2019 (Typhoon Hagibis Edition)

---
+++ FlappyBird/flappybird.py
@@ -0,0 +1,15 @@
+# http://christianthompson.com
+# Twitter: @tokyoedtech
+
+# Welcome to my "office hours" for October 10, 2019.
+# If you have any Python questions - ask in chat
+# Please provide any code via pastebin.com link
+
+# OS: Ubuntu Linux 19.04
+# Programming Editor: Geany
+
+# Live coding October 12, 2019 (Typhoon Hagibis Edition)
+# Flappy Bird Using Python 3 and the Turtle Module
+
+
+
