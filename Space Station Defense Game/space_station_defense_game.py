5ad81818c9659f3bd35611f3bcdfee67ac1af4c5
Christian Thompson <wynand1004@gmail.com>
2019-10-21 13:25:51 +0900

    Initial Commit

---
+++ Space Station Defense Game/space_station_defense_game.py
@@ -0,0 +1,15 @@
+# https://christianthompson.com
+# Twitter: @tokyoedtech
+
+# Welcome to my live coding stream for October 21, 2019.
+# If you have any Python questions - ask in chat
+# Please provide any code via pastebin.com link
+
+# OS: Ubuntu Linux 19.04
+# Programming Editor: Geany
+
+# Live coding October 21, 2019 
+# Space Station Defense Game Using Python 3 and the Turtle Module
+
+
+            
