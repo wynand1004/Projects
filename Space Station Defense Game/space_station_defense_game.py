e9267eec1d082332131509078c376f962bacc0c1
Christian Thompson <wynand1004@gmail.com>
2019-10-21 14:26:32 +0900

    Shooting the Missile

--- Space Station Defense Game/space_station_defense_game.py
+++ Space Station Defense Game/space_station_defense_game.py
@@ -15,6 +15,7 @@ import turtle
 import math
 
 wn = turtle.Screen()
+wn.setup(width=800, height=800)
 wn.title("Space Station Defense Game by @TokyoEdTech")
 wn.bgcolor("black")
 # Stop screen updates
@@ -27,28 +28,55 @@ wn.register_shape("player", player_vertices)
 class Sprite(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
+        # Maximum animation speed
+        self.speed(0)
+        self.penup()
         
 player = Sprite()
 player.color("white")
 player.shape("player")
-player.speed(0)
+
+missile = Sprite()
+missile.color("red")
+missile.shape("arrow")
+missile.speed = 0.2
+missile.state = "ready"
+missile.hideturtle()
 
 def rotate_left():
     player.lt(30)
     
 def rotate_right():
     player.rt(30)
-
+    
+def fire_missile():
+    if missile.state == "ready":
+        missile.goto(0, 0)
+        missile.showturtle()
+        missile.setheading(player.heading())
+        missile.state = "fire"
+    
 # Keyboard binding
 wn.listen()
 wn.onkey(rotate_left, "Left")
 wn.onkey(rotate_right, "Right")
+wn.onkey(fire_missile, "space")
 
 # Start main game loop
 while True:
     # Do screen update
     wn.update()
     player.goto(0, 0)
+    
+    # Move the missile
+    if missile.state == "fire":
+        missile.fd(missile.speed)
+        
+    # Border checking
+    if missile.xcor() > 400 or missile.xcor() < -400 or missile.ycor() > 400 or missile.ycor() < -400:
+        missile.hideturtle()
+        missile.state = "ready"
+
 
 
 wn.mainloop()
