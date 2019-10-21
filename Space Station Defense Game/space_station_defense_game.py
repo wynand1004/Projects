9f82ca3a2f492aafa7542c27d5b8139501e68ddc
Christian Thompson <wynand1004@gmail.com>
2019-10-21 14:14:30 +0900

    Player Rotation Done

--- Space Station Defense Game/space_station_defense_game.py
+++ Space Station Defense Game/space_station_defense_game.py
@@ -11,5 +11,44 @@
 # Live coding October 21, 2019 
 # Space Station Defense Game Using Python 3 and the Turtle Module
 
+import turtle
+import math
 
-            
+wn = turtle.Screen()
+wn.title("Space Station Defense Game by @TokyoEdTech")
+wn.bgcolor("black")
+# Stop screen updates
+wn.tracer(0)
+
+# Register Shapes
+player_vertices = ((0,15),(-15,0),(-18,5),(-18,-5),(0,0),(18,-5),(18, 5),(15, 0))
+wn.register_shape("player", player_vertices)
+
+class Sprite(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        
+player = Sprite()
+player.color("white")
+player.shape("player")
+player.speed(0)
+
+def rotate_left():
+    player.lt(30)
+    
+def rotate_right():
+    player.rt(30)
+
+# Keyboard binding
+wn.listen()
+wn.onkey(rotate_left, "Left")
+wn.onkey(rotate_right, "Right")
+
+# Start main game loop
+while True:
+    # Do screen update
+    wn.update()
+    player.goto(0, 0)
+
+
+wn.mainloop()
