cfca9158705055cd9fdec5d62f63e58032ad3e68
Christian Thompson <wynand1004@gmail.com>
2019-10-27 19:00:38 +0900

    Initial Commit

---
+++ Tetris/tetris.py
@@ -0,0 +1,11 @@
+# https://christianthompson.com
+# Twitter: @tokyoedtech
+
+# Welcome to my live coding session
+# Topic: TETRIS Part 1
+
+# OS: Ubuntu Linux 19.04
+# Programming Editor: Geany
+
+# TETRIS Using Python 3 and the Turtle Module
+
