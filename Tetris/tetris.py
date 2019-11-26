9bda0622283b1cd57af3035bc21246df79a177b7
Christian Thompson <wynand1004@gmail.com>
2019-11-26 18:54:17 +0900

    Added Shapes and Fixed Collisions
    Added various shapes and added collisions. Cleaned the code up using class methods.

--- Tetris/tetris.py
+++ Tetris/tetris.py
@@ -1,8 +1,8 @@
-# http://christianthompson.com
+# https://christianthompson.com
 # Twitter: @tokyoedtech
 
 # Welcome to my live coding session
-# Topic: TETRIS Part 1
+# Topic: TETRIS Part 2
 
 # OS: Ubuntu Linux 19.04
 # Programming Editor: Geany
@@ -27,18 +27,78 @@ class Shape():
         self.y = 0
         self.color = random.randint(1, 7)
         
+        # Block Shape
+        square = [[1,1],
+                  [1,1]]
+
+        horizontal_line = [[1,1,1,1]]
+
+        vertical_line = [[1],
+                         [1],
+                         [1],
+                         [1]]
+
+        left_l = [[1,0,0,0],
+                  [1,1,1,1]]
+                  
+        right_l = [[0,0,0,1],
+                   [1,1,1,1]]
+                   
+        left_s = [[1,1,0],
+                  [0,1,1]]
+                  
+        right_s = [[0,1,1],
+                   [1,1,0]]
+                  
+        t = [[0,1,0],
+             [1,1,1]]
+
+        shapes = [square, horizontal_line, vertical_line, left_l, right_l, left_s, right_s, t]
+
+        # Choose a random shape each time
+        self.shape = random.choice(shapes)
+
+                      
+        self.height = len(self.shape)
+        self.width = len(self.shape[0])
+        
+        # print(self.height, self.width)
+
     def move_left(self, grid):
         if self.x > 0:
             if grid[self.y][self.x - 1] == 0:
-                grid[self.y][self.x] = 0
+                self.erase_shape(grid)
                 self.x -= 1
         
     def move_right(self, grid):
-        if self.x < 11:
-            if grid[self.y][self.x + 1] == 0:
-                grid[self.y][self.x] = 0
+        if self.x < 12 - self.width:
+            if grid[self.y][self.x + self.width] == 0:
+                self.erase_shape(grid)
                 self.x += 1
-        
+    
+    def draw_shape(self, grid):
+        for y in range(self.height):
+            for x in range(self.width):
+                if(self.shape[y][x]==1):
+                    grid[self.y + y][self.x + x] = self.color
+                
+    def erase_shape(self, grid):
+        for y in range(self.height):
+            for x in range(self.width):
+                if(self.shape[y][x]==1):
+                    grid[self.y + y][self.x + x] = 0
+                    
+    def can_move(self, grid):
+        result = True
+        for x in range(self.width):
+            # Check if bottom is a 1
+            if(self.shape[self.height-1][x] == 1):
+                if(grid[self.y + self.height][self.x + x] != 0):
+                    result = False
+            
+        return result
+       
+         
 grid = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -59,10 +119,10 @@ grid = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 2, 2, 2, 4, 5, 6, 7, 4, 3, 2, 0],
-    [1, 2, 2, 2, 4, 5, 6, 7, 4, 3, 2, 0],
-    [1, 2, 2, 2, 4, 5, 6, 7, 4, 3, 2, 0],
-    [1, 2, 2, 2, 4, 5, 6, 7, 4, 3, 2, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 1, 2, 3, 0, 0, 0, 7, 1, 2, 3, 4]
 ]
 
@@ -140,13 +200,21 @@ while True:
 
     # Move the shape
     # Open Row
-    if shape.y == 23:
+    # Check for the bottom
+    if shape.y == 23 - shape.height + 1:
         shape = Shape()
         check_grid(grid)
-    elif grid[shape.y + 1][shape.x] == 0:
-        grid[shape.y][shape.x] = 0
+    # Check for collision with next row
+    elif shape.can_move(grid):
+        # Erase the current shape
+        shape.erase_shape(grid)
+        
+        # Move the shape by 1
         shape.y +=1
-        grid[shape.y][shape.x] = shape.color
+        
+        # Draw the shape again
+        shape.draw_shape(grid)
+
     else:
         shape = Shape()
         check_grid(grid)
