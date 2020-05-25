529923d17dc7f5d710cc9c0af9301d4d4645893d
Christian Thompson <wynand1004@gmail.com>
2020-05-25 12:50:08 +0900

    Initial Commit
    From my Java to Python Tutorial on YouTube

---
+++ JavaToPython/java_to_python12.py
@@ -0,0 +1,39 @@
+# ChristianThompson.com
+# @TokyoEdTech
+# Lesson 1 & 2: Comments and Printing
+
+# This is a multiline comment!
+# This is a multiline comment!
+# This is a multiline comment!
+
+print("Lesson 1 & 2: Comments and Printing")
+print("Hello world!")
+print("Hello" + "world!")
+print("Hello" + " " + "world!")
+
+print("")
+print(2)
+print("2")
+
+print("")
+print(2.0)
+print("2.0")
+
+print("");
+print(2 + 2);
+print("2 + 2");
+print("2" + "2");
+
+print("");
+print(2.0 + 2.0);
+print("2.0 + 2.0");
+print("2.0" + "2.0");
+
+print("\nMy name is ", end='');
+print("Christian.");
+
+print("\tMy name is ", end='');
+print("Christian.");
+
+print("\nMy Twitter ID is ", end='');
+print("\"TokyoEdTech\".");
