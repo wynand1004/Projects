97244f0e489a2341a27c697e37123758441d41a3
Christian Thompson <wynand1004@gmail.com>
2020-06-17 13:45:25 +0900

    Initial Commit

---
+++ Game State/game_state_starter.py
@@ -0,0 +1,53 @@
+# Game State Demo (Splash Screen, Game, End Screen)
+# Python 3.x Compatible
+# Windows, MacOSX, and Linux Compatible
+# by @TokyoEdTech
+# Blog: https://www.christianthompson.com
+# YouTube Channel: https://www.youtube.com/channel/UC2vm-0XX5RkWCXWwtBZGOXg/
+
+# Import modules
+import turtle
+import math
+import random
+import time
+
+# Create classes
+
+
+# Create functions
+
+
+# Define constants
+SCREEN_WIDTH = 800  
+SCREEN_HEIGHT = 600
+
+
+# Game Setup
+wn = turtle.Screen()
+wn.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
+wn.title("Game State Demo by TokyoEdTech")
+wn.bgcolor("black")
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+
+# Keyboard Bindings
+
+
+# Main Game Loop
+while True:
+        
+    # Clear the screen
+    pen.clear()
+    
+    # Game code here
+
+
+
+    # Update the screen
+    wn.update()
+
