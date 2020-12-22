504ce1f3620f0818eb7ae5dbe8667f678980aa3f
Christian Thompson <wynand1004@gmail.com>
2020-12-22 16:22:07 +0900

    Enemies Die, Phasers Fire, and Enemies Fire
    Enemies can now die (shield strength 0)
    Phasers are a class and render better
    Phasers fire with left shift key
    Enemies fire randomly

--- VectTrek/vectrek.py
+++ VectTrek/vectrek.py
@@ -1,24 +1,22 @@
 # VecTrek Technology Demo
 # Python 3.x Compatible
 # Windows, MacOSX, and Linux Compatible
-# by @TokyoEdTech
+# by @TokyoEdtech
 
-# 16-Bit Member Shoutouts: Kevin, Paul, and Jan
-
-# Blog: https://www.christianthompson.com
-# YouTube Channel: https://www.youtube.com/channel/UC2vm-0XX5RkWCXWwtBZGOXg/
+# Snake Shoutouts: Kevin & Paul
+# Invader Shoutouts: Jan, Mohd, Kim-Siong, and Finnesz
 
 import turtle
 import math
 import random
 import time
 
-SCREEN_WIDTH = 800  
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1200  
+SCREEN_HEIGHT = 800
 
 wn = turtle.Screen()
 wn.setup(width = SCREEN_WIDTH+20, height = SCREEN_HEIGHT + 20)
-wn.title("VecTrek Technology Demo by TokyoEdTech")
+wn.title("VecTrek Technology Demo by TokyoEdtech")
 wn.bgcolor("black")
 
 pen = turtle.Turtle()
@@ -37,6 +35,9 @@ class VectorShape():
         self.scale = scale
         self.active = True
         self.color = "white"
+        self.phaser_power = 100
+        self.photon_torpedos = 1
+        self.shields = 100
         
         min_x = self.shape[0][0]
         max_x = self.shape[0][0]
@@ -118,6 +119,19 @@ class Starship(VectorShape):
         if self.y < -SCREEN_HEIGHT / 2.0:
             self.y = SCREEN_HEIGHT / 2.0
 
+    def fire_phaser(self, enemy):
+        min_distance = 10000
+        target = None
+        for vector in vectors:
+            if isinstance(vector, Enemy):
+                distance_to_enemy = vector.get_distance(player)
+                if distance_to_enemy < min_distance:
+                    min_distance = distance_to_enemy
+                    target = vector
+        if target:
+            phaser.fire(self, target)
+            target.shields -= 10     
+
 class PhotonTorpedo(VectorShape):
     def __init__(self, shape, x, y, dx, dy, h, dh, scale):
         VectorShape.__init__(self, shape, x, y, h, scale)
@@ -155,6 +169,7 @@ class Enemy(VectorShape):
         self.active = True
         self.color = "green"
         
+        
     def move(self, SCREEN_WIDTH, SCREEN_HEIGHT):
         self.x += self.dx
         self.y += self.dy
@@ -171,10 +186,56 @@ class Enemy(VectorShape):
             self.y = -SCREEN_HEIGHT / 2.0
         if self.y < -SCREEN_HEIGHT / 2.0:
             self.y = SCREEN_HEIGHT / 2.0
+            
+        # Check status
+        if self.shields <= 0:
+            self.destroy()
+            
+    def destroy(self):
+        self.active = False
+        self.x = -100000
+        self.y = -100000
+
+class Phaser():
+    def __init__(self):
+        self.v1 = None
+        self.v2 = None
+        self.timer = 0
+        self.active = False
+
+    def fire(self, v1, v2):
+        self.v1 = v1
+        self.v2 = v2
+        self.timer = 10
+        self.active = True
+
+    def render(self, pen):
+        if self.active and self.timer > 0:
+            pen.penup()
+            pen.goto(self.v1.x, self.v1.y)
+            pen.color("red")
+            pen.pendown()
+            pen.pensize(3)
+            pen.goto(self.v2.x, self.v2.y)
+            pen.penup()
+            pen.pensize(1)
+            self.timer -= 1
+        else:
+            self.reset()
+            
+    def reset(self):
+        self.v1 = None
+        self.v2 = None
+        self.timer = 0
+        self.active = 0
+        
+    def move(self, SCREEN_WIDTH, SCREEN_HEIGHT):
+        pass
 
 starship_shape = ((-8, 4), (-2, 4), (), (-8, -4), (-2, -4), (), (-5, 3), (-3, 1), (), (-3, -1), (-5, -3), (), (-5, 0), (2, 0), (5, 3), (8, 0), (5, -3), (2,0))
 photon_torpedo_shape = ((-2, 0), (-1, 1), (0, -1), (1, 1), (2, 0))
 bird_of_prey_shape = ((-8,0),(-5,5),(-2,0),(-5,-5),(-8,0),(),(-7,6),(-3,6),(),(-7,-6),(-3,-6),(),(-2,0),(2,0),(4,2),(6,0),(4,-2),(2,0))
+phaser = Phaser()
 
 wn.tracer(0)
 
@@ -190,13 +251,16 @@ def player_accelerate():
 def player_decelerate():
     player.speed -= 0.1
 
-def player_fire():
+def player_fire_photon():
     if photon_torpedo.active == False:
         photon_torpedo.x = player.x
         photon_torpedo.y = player.y
         photon_torpedo.h = player.h
-        photon_torpedo.speed = 0.5
+        photon_torpedo.speed = 1
         photon_torpedo.active = True
+        
+def player_fire_phaser():
+    player.fire_phaser(vectors)
     
 # Keyboard Binding
 wn.listen()
@@ -204,14 +268,16 @@ wn.onkeypress(player_left, "Left")
 wn.onkeypress(player_right, "Right")
 wn.onkeypress(player_accelerate, "Up")
 wn.onkeypress(player_decelerate, "Down")
-wn.onkeypress(player_fire, "space")
+wn.onkeypress(player_fire_photon, "space")
+wn.onkeypress(player_fire_phaser, "Shift_L")
 
 player = Starship(starship_shape, 0, 0, 0, 0, 90, 0, 2.0)
 photon_torpedo = PhotonTorpedo(photon_torpedo_shape, 0, 0, 0, 0, 0, 0, 1.0)
+enemy_torpedo = PhotonTorpedo(photon_torpedo_shape, 0, 0, 0, 0, 0, 0, 1.0)
 
-vectors = [player, photon_torpedo]
+vectors = [player, photon_torpedo, phaser, enemy_torpedo]
 
-for _ in range(3):
+for _ in range(10):
     x = random.randint(-300, 300)
     y = random.randint(-300, 300)
     h = random.randint(0, 360)
@@ -229,33 +295,20 @@ while True:
 
         if isinstance(vector, Enemy):
             if vector.is_aabb_collision(photon_torpedo):
-                vector.x = random.randint(-300, 300)
-                vector.y = random.randint(-300, 300)
+                vector.shields -= 50
                 photon_torpedo.x = 1000
                 photon_torpedo.y = 1000
                 photon_torpedo.active = False
-
-    # Fire phaser demo
-    if random.randint(0, 100) > 98:
-        min_distance = 10000
-        target = None
-        for vector in vectors:
-            if isinstance(vector, Enemy):
-                distance_to_enemy = vector.get_distance(player)
-                if distance_to_enemy < min_distance:
-                    min_distance = distance_to_enemy
-                    target = vector
-                print(vector.get_distance(player))
-        if target:        
-            pen.penup()
-            pen.goto(player.x, player.y)
-            pen.color("red")
-            pen.pendown()
-            pen.pensize(3)
-            pen.goto(target.x, target.y)
-            pen.penup()
-            pen.pensize(1)
-            
+                
+    # Fire enemy torpedo
+    if enemy_torpedo.active == False:
+        vector = random.choice(vectors)
+        if isinstance(vector, Enemy) and vector.get_distance(player) < 200:
+            enemy_torpedo.x = vector.x
+            enemy_torpedo.y = vector.y
+            enemy_torpedo.h = vector.h
+            enemy_torpedo.speed = 1
+            enemy_torpedo.active = True
 
     # Update the screen
     wn.update()
