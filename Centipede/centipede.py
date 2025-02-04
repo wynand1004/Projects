caa3fc2421e56ee3fbe4cf92382399980e6cd6ca
Christian Thompson <wynand1004@gmail.com>
2025-02-04 19:25:46 +0900

    Classes Updated  / Game Code Simplified
    Added Sprite Class
    Added Score
    Combined code

--- Centipede/centipede.py
+++ Centipede/centipede.py
@@ -4,12 +4,33 @@ import random
 import math
 
 # Classes
-class Segment:
-    def __init__(self, x, y, color="white"):
+class Sprite:
+    def __init__(self, x, y, color="white", shape="square"):
         self.x = x
         self.y = y
+        self.dx = 0
+        self.dy = 0
+        self.speed = 0
         self.color = color
-        self.shape = "circle"
+        self.shape = shape
+        
+    def goto(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def move(self):
+        self.x += self.dx
+        self.y += self.dy
+        
+    def is_collision(self, other):
+        a = self.x - other.x
+        b = self.y - other.y
+        d = math.sqrt(a**2 + b**2)
+        
+        if d<20:
+            return True
+        else:
+            return False
         
     def render(self, pen):
         pen.goto(self.x, self.y)
@@ -17,6 +38,10 @@ class Segment:
         pen.shape(self.shape)
         pen.stamp()
 
+class Segment(Sprite):
+    def __init__(self, x, y, color="green"):
+        super().__init__(x, y, color, "circle")
+                
 class Centipede:
     def __init__(self, x, y, segments = [], color="green", size = 1):
         self.x = x
@@ -93,7 +118,6 @@ class Centipede:
             else:
                 self.direction = "right"
                 
-        
         # Check for border collision
         if head.x > right_border or head.x < left_border:
             head.x -= dx
@@ -103,30 +127,24 @@ class Centipede:
         for segment in self.body:
             segment.render(pen)
 
-class Mushroom:
-    def __init__(self, x, y, color="white"):
-        self.x = x
-        self.y = y
+class Mushroom(Sprite):
+    def __init__(self, x, y, color="white", shape="square"):
+        super().__init__(x, y, color, shape)
         self.health = 3
         self.colors = ("black", "red", "yellow", "white")
-        self.shape = "square"
         
-    def render(self, pen):
-        pen.goto(self.x, self.y)
-        pen.color(self.colors[self.health])
-        pen.shape(self.shape)
-        pen.stamp()
-        
-class Player:
-    def __init__(self, x, y, color="yellow"):
-        self.x = x
-        self.y = y
+    def register_hit(self):
+        self.health -= 1
+        if self.health < 0:
+            self.health = 0
+        self.color = self.colors[self.health]
+
+class Player(Sprite):
+    def __init__(self, x, y, color="yellow", shape="arrow"):
+        super().__init__(x, y, color, shape)
         self.lives = 3
-        self.color = color
-        self.dx = 0
-        self.dy = 0
         self.speed = 2.5
-        self.shape = "arrow"
+        self.score = 0
         
     def go_up(self):
         self.dy = self.speed
@@ -159,50 +177,19 @@ class Player:
         if self.y > -100:
             self.y = -100
             self.dy = 0
-        
-    def render(self, pen):
-        pen.goto(self.x, self.y)
-        pen.color(self.color)
-        pen.shape(self.shape)
-        pen.stamp()
 
-class Weapon():
-    def __init__(self, x, y, color="yellow"):
-        self.x = x
-        self.y = y
-        self.color = color
+class Weapon(Sprite):
+    def __init__(self, x, y, color="yellow", shape="arrow"):
+        super().__init__(x, y, color, shape)
         self.dy = 10
     
     def fire(self):
-        self.goto(player.x, player.y)
-    
-    def goto(self, x, y):
-        self.x = x
-        self.y = y
-        
-    def move(self):
-        self.y += self.dy
-        
-    def is_collision(self, other):
-        a = self.x - other.x
-        b = self.y - other.y
-        a *= a
-        b *= b
-        d = math.sqrt(a + b)
-        
-        if d<20:
-            return True
-        else:
-            return False
-        
-    def render(self, pen):
-        pen.goto(self.x, self.y)
-        pen.color(self.color)
-        pen.stamp()    
-
+        if self.y > 400:
+            self.goto(player.x, player.y)
+                
 # Set up the screen
 wn = turtle.Screen()
-wn.title("Centipede by @TokyoEdTech")
+wn.title("Centipede by @TokyoEdtech")
 wn.bgcolor("black")
 wn.setup(width=600, height=800)
 wn.tracer(0)
@@ -221,17 +208,17 @@ centipedes = [Centipede(-280, 380, [], "green", 20)]
 
 # Create mushroom(s)
 mushrooms = []
-for _ in range(10):
+for _ in range(25):
     x = random.randrange(-300, 300, 20)
     y = random.randrange(-100, 380, 20)
     mushrooms.append(Mushroom(x, y, "white"))
     print(x, y)
 
 # Create player
-player = Player(0, -300)
+player = Player(0, -300, "yellow", "arrow")
 
 # Create weapon
-weapon = Weapon(0, 1000)
+weapon = Weapon(0, 1000, "yellow", "arrow")
 
 # Game variables
 delay = 0
@@ -252,6 +239,7 @@ while True:
     wn.update()
     pen.clear()
     
+    # Move centipedes
     if frame == 0:
         for centipede in centipedes:
             centipede.move(mushrooms)
@@ -259,53 +247,55 @@ while True:
     if frame == max_frames:
         frame = 0
     
-    for centipede in centipedes:
-        centipede.render(pen)
-
-    for mushroom in mushrooms:
-        mushroom.render(pen)
+    # Render game objects
+    game_objects = centipedes + mushrooms + [player, weapon]
     
+    for game_object in game_objects:
+        game_object.render(pen)
+    
+    # Move player and weapon
     player.move()
-    player.render(pen)
-    
     weapon.move()
-    weapon.render(pen)
     
     # Check for weapon collisions with mushrooms
-    to_be_deleted = []
-    
-    for mushroom in mushrooms:
+    for i in range(len(mushrooms)-1, -1, -1):
+        mushroom = mushrooms[i]
         if weapon.is_collision(mushroom):
-            mushroom.health -= 1
+            mushroom.register_hit()
             weapon.y = 1000
             
             if mushroom.health == 0:
-                to_be_deleted.append(mushroom)
-    
-    for mushroom in to_be_deleted:
-        mushrooms.remove(mushroom)
+                mushrooms.remove(mushroom)
         
     # Check for collisions with segments
-    for centipede in centipedes:
+    for i in range(len(centipedes)-1, -1, -1):
+        centipede = centipedes[i]
         for segment in centipede.body:
             if weapon.is_collision(segment):
                 # Check if it is the head
                 if centipede.body.index(segment) == 0:
                     centipede.body.pop(0)
+                    mushrooms.append(Mushroom(segment.x, segment.y))
                     if len(centipede.body) > 0:
                         centipede.body[0].color = "red"
+                    else:
+                        centipedes.remove(centipede)
                 else:
                     segment.color = "red"
                     weapon.y = 1000
                     mushrooms.append(Mushroom(segment.x, segment.y))
 
-    for centipede in centipedes:
-        segments = centipede.spawn()
-        if segments != None and len(segments) > 0:
-            x = segments[0].x
-            y = segments[0].y
-            centipedes.append(Centipede(x, y, segments))
+                    segments = centipede.spawn()
+                    if segments != None and len(segments) > 0:
+                        x = segments[0].x
+                        y = segments[0].y
+                        centipedes.append(Centipede(x, y, segments))
             
     for i in range(len(centipedes)-1, -1, -1):
         if len(centipedes[i].body) == 0:
             centipedes.remove(centipedes[i])
+            
+    # Draw the score
+    pen.goto(-280, 360)
+    pen.color("white")
+    pen.write(f"Score: {player.score}", move=False, align='left', font=('Courier New', 24, 'normal')) 
